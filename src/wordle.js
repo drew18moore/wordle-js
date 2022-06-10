3992b2cba64ba48d36a8186b3ef42e3c2698fc3d
Drew <dcm752052@gmail.com>
2022-06-09 21:15:51 -0400

    Created functionless keyboard

--- src/wordle.js
+++ src/wordle.js
@@ -1,5 +1,12 @@
+const keys = [
+    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
+    "A", "S", "D", "F", "G", "H", "J", "K", "L", 
+    "ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"
+];
+
 window.onload = function() {
     createBoard();
+    createKeyboard();
 }
 
 function createBoard() {
@@ -9,5 +16,14 @@ function createBoard() {
             tile.classList.add("tile");
             document.getElementById("board").appendChild(tile);
         }
+    }
+}
+
+function createKeyboard() {
+    for (let i = 0; i < keys.length; i++) {
+        let key = document.createElement("div");
+        key.classList.add("key");
+        key.innerHTML = keys[i];
+        document.getElementById("keyboard").appendChild(key);
     }
 }
