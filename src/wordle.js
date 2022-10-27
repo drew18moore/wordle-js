b8c6a762f036e86cb261ba7e089ffbc691af3c8a
Drew <dcm752052@gmail.com>
2022-10-26 20:11:50 -0400

    When the user types a word and presses enter, the API will return true/false weather or not word is a real word.

--- src/wordle.js
+++ src/wordle.js
@@ -1,7 +1,32 @@
 const keys = [
-    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
-    "A", "S", "D", "F", "G", "H", "J", "K", "L", 
-    "ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "ENTER",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "DELETE",
 ];
 
 let word = "crane";
@@ -9,16 +34,16 @@ let user_input = [];
 let numGreenTiles = 0;
 let turn = 0;
 
-window.onload = function() {
+window.onload = function () {
     createBoard();
     createKeyboard();
     fetchWord();
-}
+};
 
 function fetchWord() {
-    fetch ('https://thatwordleapi.azurewebsites.net/get/')
+    fetch("https://thatwordleapi.azurewebsites.net/get/")
         .then((response) => response.json())
-        .then((data) => word = data.Response);
+        .then((data) => (word = data.Response));
 }
 
 function createBoard() {
@@ -26,7 +51,7 @@ function createBoard() {
         for (let j = 0; j < 5; j++) {
             let tile = document.createElement("div");
             tile.classList.add("tile");
-            tile.innerHTML = user_input
+            tile.innerHTML = user_input;
             document.getElementById("board").appendChild(tile);
         }
     }
@@ -78,7 +103,11 @@ function createKeyboard() {
 
 function selectKey() {
     console.log("USER PRESSED", this.innerHTML);
-    if (this.innerHTML != "ENTER" && this.innerHTML != "DELETE" && user_input.length < 5) {
+    if (
+        this.innerHTML != "ENTER" &&
+        this.innerHTML != "DELETE" &&
+        user_input.length < 5
+    ) {
         user_input.push(this.innerHTML);
         console.log(user_input);
         updateBoard();
@@ -88,43 +117,64 @@ function selectKey() {
         updateBoard();
     } else if (this.innerHTML == "ENTER" && user_input.length == 5) {
         checkWord();
-        user_input = [];
-        turn++;
-        if (turn > 5) {
-            console.log("YOU LOSE");
-            freezeKeyboard();
-        }
     }
 }
 
 function updateBoard() {
     let allTiles = document.querySelectorAll(".tile");
     allTiles = [...allTiles];
-    for(let i = 0; i < 5; i++) {
-        allTiles[i + turn*5].innerHTML = ((user_input.length -1 < i) ? "" : user_input[i]);
+    for (let i = 0; i < 5; i++) {
+        allTiles[i + turn * 5].innerHTML =
+            user_input.length - 1 < i ? "" : user_input[i];
     }
 }
 
 function checkWord() {
-    let allTiles = document.querySelectorAll(".tile");
-    allTiles = [...allTiles];
-    for (let i = 0; i < user_input.length; i++) {
-        if (word.toUpperCase()[i] == user_input[i]) {
-            allTiles[i + turn*5].classList.add("green");
-            numGreenTiles++;
-            console.log("NUM GREEN TILES", numGreenTiles);
-        } else if (word.toUpperCase().includes(user_input[i]) && word.toUpperCase()[i] != user_input[i]) {
-            allTiles[i + turn*5].classList.add("yellow");
-        }
-    }
+    fetch(
+        `https://thatwordleapi.azurewebsites.net/ask/?word=${user_input
+            .join("")
+            .toLowerCase()}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.Response === false) {
+                console.log("NOT A WORD");
+                user_input = [];
+                updateBoard();
+                return;
+            } else {
+                let allTiles = document.querySelectorAll(".tile");
+                allTiles = [...allTiles];
+                for (let i = 0; i < user_input.length; i++) {
+                    if (word.toUpperCase()[i] == user_input[i]) {
+                        allTiles[i + turn * 5].classList.add("green");
+                        numGreenTiles++;
+                        console.log("NUM GREEN TILES", numGreenTiles);
+                    } else if (
+                        word.toUpperCase().includes(user_input[i]) &&
+                        word.toUpperCase()[i] != user_input[i]
+                    ) {
+                        allTiles[i + turn * 5].classList.add("yellow");
+                    }
+                }
 
-    if (numGreenTiles == 5) {
-        console.log("YOU WIN");
-        freezeKeyboard();
+                if (numGreenTiles == 5) {
+                    console.log("YOU WIN");
+                    freezeKeyboard();
+                } else {
+                    numGreenTiles = 0;
+                }
 
-    } else {
-        numGreenTiles = 0;
-    }
+                turn++;
+                user_input = [];
+            }
+        })
+        .then(() => {
+            if (turn > 5) {
+                console.log("YOU LOSE");
+                freezeKeyboard();
+            }
+        });
 }
 
 function freezeKeyboard() {
